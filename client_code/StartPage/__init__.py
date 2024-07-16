e0cff66cdcd8e5c380673656fad259f1c896a589
alec.ells123@gmail.com <alec.ells123@gmail.com>
2024-07-16 17:53:00 +0000

    Edited StartPage

--- client_code/StartPage/__init__.py
+++ client_code/StartPage/__init__.py
@@ -24,30 +24,12 @@ class StartPage(StartPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.state = "Start"
 
     # Any code you write here will run when the form opens.
 
-  def submit_button_click(self, **event_args):
-    # This method runs when the button is clicked.
-    # First, we grab the contents of the text boxes:
-    name = self.name_box.text
-    email = self.email_box.text
-    feedback = self.feedback_box.text
-
-    # Now we call our Server Module to save our input
-    # in the database and send you an email:
-    anvil.server.call("add_feedback", name, email, feedback)
-    # (Hint: Find ServerModule1 under "Server Code" on the
-    # left. Click on the folder icon if you can't see it.)
-
-    # Display something to the user so they know it worked:
-    Notification("Feedback submitted!").show()
-    self.clear_inputs()
-
   def clear_inputs(self):
-    self.name_box.text = ""
-    self.email_box.text = ""
-    self.feedback_box.text = ""
+    pass
 
   def new_player_button_click(self, **event_args):
     self.new_player_card.visible = True

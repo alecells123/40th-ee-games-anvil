eca3cbb481227279c598db219a0b5a2921d2900e
alec.ells123@gmail.com <alec.ells123@gmail.com>
2024-07-17 19:11:20 +0000

    Edited settings, ServerModule1, StartPage.ItemTemplate1 and StartPage

--- client_code/StartPage/__init__.py
+++ client_code/StartPage/__init__.py
@@ -1,7 +1,6 @@
 from ._anvil_designer import StartPageTemplate
 from anvil import *
 import plotly.graph_objects as go
-import anvil.users
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,5 +1,3 @@
-import anvil.users
-import anvil.email
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables

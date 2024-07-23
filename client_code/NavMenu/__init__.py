c2f2b97c872d77e862999d8830e82ffd9d5b50d4
alec.ells123@gmail.com <alec.ells123@gmail.com>
2024-07-23 18:01:18 +0000

    Created PlayersRepeatable; Edited settings, PlayersPage and NavMenu

--- client_code/NavMenu/__init__.py
+++ client_code/NavMenu/__init__.py
@@ -10,3 +10,13 @@ class NavMenu(NavMenuTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+  def character_tab_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if properties['tab'] != 'CHARACTER':
+      open_form('CharacterPage')
+
+  def players_tab_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if properties['tab'] != 'PLAYERS':
+      open_form('PlayersPage')

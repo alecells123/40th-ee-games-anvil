896ba33658dd71731c53e465efde2b6efae8b930
alec.ells123@gmail.com <alec.ells123@gmail.com>
2024-07-27 13:55:02 +0000

    Edited SponsorMenuPage

--- client_code/SponsorMenuPage.py
+++ client_code/SponsorMenuPage.py
@@ -17,3 +17,31 @@ class SponsorMenuPage(SponsorMenuPageTemplate):
   def update_tab(self, menu):
     self.menu_items = anvil.server.call('get_menu', menu)
     self.refresh_data_bindings()
+
+  def dusty_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def delivery_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def button_1_copy_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def drugs_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def information_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def medical_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def nutritional_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass

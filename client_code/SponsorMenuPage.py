05824f22f5868bae8b5d07858e11ac17f15a1686
alec.ells123@gmail.com <alec.ells123@gmail.com>
2024-07-28 17:43:06 +0000

    Edited SponsorMenuPage

--- client_code/SponsorMenuPage.py
+++ client_code/SponsorMenuPage.py
@@ -14,7 +14,7 @@ class SponsorMenuPage(SponsorMenuPageTemplate):
     # Any code you write here will run when the form opens.
 
   def update_tab(self, menu):
-    self.repeating_panel_1.items = anvil.server.call('get_menu', menu)
+    self.menu_repeating_panel.items = anvil.server.call('get_menu', menu)
     self.refresh_data_bindings()
 
   def dusty_button_click(self, **event_args):

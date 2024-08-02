98c593539474cd6eedf4c2043794fa202a219bb0
Alec Ellsworth <alec.ells123@gmail.com>
2024-08-02 18:03:32 -0500

    asdfasdf

--- client_code/ContestantsPrintable/ContestantsPrintRepeatable/__init__.py
+++ client_code/ContestantsPrintable/ContestantsPrintRepeatable/__init__.py
@@ -12,5 +12,34 @@ class ContestantsPrintRepeatable(ContestantsPrintRepeatableTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    self.name_label.text = self.item['name']
+    self.name_label.text = self.item.get('name', '')
+    self.relationships_label.text = self.item.get('relationships', '')
+    self.goals_label.text = self.item.get('goals', '')
 
+    self.primary_drug_label.text = self.item.get('primary_drug', '')
+    self.ability_name_label.text = self.item.get('ability_name', '')
+
+    self.max_injuries_label.text = self.item.get('max_injuries', '')
+    self.max_sanity_label.text = self.item.get('max_sanity', '')
+    
+    # Additional fields from the YAML file
+    self.injuries_label.text = self.item.get('injuries', '')
+    self.drug_label.text = self.item.get('drug', '')
+    self.sanity_label.text = self.item.get('sanity', '')
+    self.behavious_label.text = self.item.get('behavious', '')
+    self.defense_label.text = self.item.get('defense', '')
+    self.initiative_label.text = self.item.get('initiative', '')
+    self.strength_label.text = self.item.get('strength', '')
+    self.convince_label.text = self.item.get('convince', '')
+    self.brawl_label.text = self.item.get('brawl', '')
+    self.coordination_label.text = self.item.get('coordination', '')
+    self.engineer_label.text = self.item.get('engineer', '')
+    self.shoot_label.text = self.item.get('shoot', '')
+    self.intellect_label.text = self.item.get('intellect', '')
+    self.know_label.text = self.item.get('know', '')
+    self.strike_label.text = self.item.get('strike', '')
+    self.social_label.text = self.item.get('social', '')
+    self.perceive_label.text = self.item.get('perceive', '')
+    self.throw_label.text = self.item.get('throw', '')
+    self.utilize_label.text = self.item.get('utilize', '')
+    self.subskills_label.text = self.item.get('subskills', '')

a37746816fc39d7123210290ce902f670b5aa34e
alec.ells123@gmail.com <alec.ells123@gmail.com>
2024-08-02 23:20:38 +0000

    Edited NavMenu

--- client_code/NavMenu/__init__.py
+++ client_code/NavMenu/__init__.py
@@ -10,7 +10,7 @@ class NavMenu(NavMenuTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    if na
+    if n
 
   def character_tab_click(self, **event_args):
     """This method is called when the button is clicked"""

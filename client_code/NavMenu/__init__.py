b60cd6717b0bb732c5242850f2aeaf11da609b68
alec.ells123@gmail.com <alec.ells123@gmail.com>
2024-08-02 23:20:36 +0000

    Edited AgendaAdmin and NavMenu

--- client_code/NavMenu/__init__.py
+++ client_code/NavMenu/__init__.py
@@ -10,6 +10,7 @@ class NavMenu(NavMenuTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    if na
 
   def character_tab_click(self, **event_args):
     """This method is called when the button is clicked"""
